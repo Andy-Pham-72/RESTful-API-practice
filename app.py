d3ef5e923ae507717e1e7e2b2b73976b98519571
Andy Pham <aqpham02@gmail.com>
2022-01-02 23:22:54 -0500

    update_planet using PUT method

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 jwt = JWTManager(app)
-mail= Mail(app)
+mail = Mail(app)
 
 
 @app.cli.command("dc_create")
@@ -163,6 +163,58 @@ def retrieve_password(email: str):
         return jsonify(message='Password sent to ' + email)
     else:
         return jsonify(message="That mail doesn't exist!"), 401
+
+
+@app.route('/planet_details/<int:planet_id>', methods=['GET'])
+def planet_details(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        result = planet_schema.dump(planet)
+        return jsonify(result)
+    else:
+        return jsonify(message="That planet doesn't exist!"), 404
+
+
+@app.route("/add_planet", methods=['POST'])
+@jwt_required()
+def add_planet():
+    planet_name = request.form['planet_name']
+    test = Planet.query.filter_by(planet_name=planet_name).first()
+    if test:
+        return jsonify("There is already a planet by that name"), 409
+    else:
+        planet_type = request.form['planet_type']
+        home_star = request.form['home_star']
+        mass = float(request.form['mass'])
+        radius = float(request.form['radius'])
+        distance = float(request.form['distance'])
+
+        new_planet = Planet(planet_name=planet_name,
+                            planet_type=planet_type,
+                            home_star=home_star,
+                            mass=mass,
+                            radius=radius,
+                            distance=distance)
+
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify(message='You added a planet!'), 201
+
+
+@app.route('/update_planet', methods=['PUT'])
+def update_planet():
+    planet_id = int(request.form['planet_id'])
+    planet = Planet.query.filter_by(planet_id = planet_id).first()
+    if planet:
+        planet.planet_type = request.form['planet_type']
+        planet.home_star = request.form['home_star']
+        planet.mass = float(request.form['mass'])
+        planet.radius = float(request.form['radius'])
+        planet.distance = float(request.form['distance'])
+        db.session.commit()
+        return jsonify(message="You updated planet"), 202
+    else:
+        return jsonify(message="That planet doesn't exist"), 404
 
 
 # database models
